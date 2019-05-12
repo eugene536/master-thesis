0f0bbf7608b9b9994a1da0eff18cbf7435157eb3
Eugene <n.eugene536@gmail.com>
2019-05-12 23:28:38 +0300

    [chapter2] parsing of lambda

--- r.sh
+++ r.sh
@@ -4,10 +4,11 @@ if [[ "$1" == "clean" ]]; then
     rm -f master-thesis*.{aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
     rm -f *.log
 else
-    xelatex master-thesis
+    cd /home/eugene/university/master-thesis
+    xelatex -interaction=nonstopmode master-thesis
     biber   master-thesis
-    xelatex master-thesis
-    xelatex master-thesis
+    xelatex -interaction=nonstopmode master-thesis
+    xelatex -interaction=nonstopmode master-thesis
 
     rm -f master-thesis.{aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
 fi
