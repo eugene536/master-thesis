a850ec094533d2335b7616e92baf45f4ea85359f
Eugene <n.eugene536@gmail.com>
2019-05-23 18:50:54 +0300

    add script for converting thesis to gray colors

---
+++ convert_to_gray.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gs \
+ -sOutputFile=gray.pdf \
+ -sDEVICE=pdfwrite \
+ -sColorConversionStrategy=Gray \
+ -dProcessColorModel=/DeviceGray \
+ -dCompatibilityLevel=1.4 \
+ -dNOPAUSE \
+ -dBATCH \
+ master-thesis.pdf
